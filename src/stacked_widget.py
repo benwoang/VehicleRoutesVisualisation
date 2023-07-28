59e842e0d5170768451280c65e92e420f7e09158
Ben Woang <ben.woang19@gmail.com>
2023-07-28 10:13:54 +1000

    Stacked Layout implementation

--- src/stacked_widget.py
+++ src/stacked_widget.py
@@ -4,8 +4,10 @@ from PyQt6.QtWidgets import (
     QWidget,
     QMainWindow,
     QVBoxLayout,
+    QHBoxLayout,
     QStackedWidget,
     QPushButton,
+    QLabel,
 )
 from solution_visualiser import SolutionVisualiser
 from map_visualiser import MapVisualiser
@@ -28,21 +30,44 @@ from map_visualiser import MapVisualiser
 class StackedWidget(QWidget):
     def __init__(self):
         super().__init__()
+        # Setup base
+        self.base_layout = QVBoxLayout()
+        self.stackedWidget = QStackedWidget()
+        self.base_layout.addWidget(self.stackedWidget)
+        self.stackedWidget.addWidget(self.create_map_page())
+        self.stackedWidget.addWidget(self.create_solution_page())
+        # layout = QVBoxLayout()
+        # self.fig_can = MapVisualiser("31x79-w5.map")
+        self.button = QPushButton("switch", self)
+        self.button.clicked.connect(self.switch_layout)
+        self.base_layout.addWidget(self.button)
 
-        # self.stackedWidget = QStackedWidget()
-        layout = QVBoxLayout(self)
+        # layout.addWidget(self.button)
+        self.setLayout(self.base_layout)
+        self.stackedWidget.setCurrentIndex(1)
+
+    def create_map_page(self):
+        map_page = QWidget()
+        layout = QVBoxLayout(map_page)
         self.fig_can = MapVisualiser("31x79-w5.map")
         layout.addWidget(self.fig_can)
-        self.button = QPushButton("test", self)
+        self.button = QPushButton("Print", self)
+        self.button.clicked.connect(self.switch_layout)
         layout.addWidget(self.button)
-
-    # def create_stacked_widget(self):
-    #     self.stackedWidget = QStackedWidget()
-    def create_map_page(self):
-        pass
+        return map_page
 
     def create_solution_page(self):
-        pass
+        solution_page = QWidget()
+        layout = QVBoxLayout(solution_page)
+        label = QLabel("My text")
+        layout.addWidget(label)
+        return solution_page
+
+    def switch_layout(self):
+        if self.stackedWidget.currentIndex() == 0:
+            self.stackedWidget.setCurrentIndex(1)
+        else:
+            self.stackedWidget.setCurrentIndex(0)
 
 
 if __name__ == "__main__":
